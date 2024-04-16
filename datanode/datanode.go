bc96fa82dface9f0d426df592e41945ee1ff5f44
David Han <dmh338@cornell.edu>
2024-04-16 15:59:53 -0400

    Add heartbeat rpc

--- datanode/datanode.go
+++ datanode/datanode.go
@@ -1,2 +1,21 @@
 package datanode
 
+import (
+	"log"
+	"errors"
+)
+
+type DataNodeService struct {
+	Port uint64
+	NameNodePort uint64
+}
+
+func Heartbeat(req bool, res *bool) error {
+	if req {
+		log.Println("received from Namenode")
+		*res = true
+		return nil
+	}
+
+	return errors.New("heartbeat request error")
+} 
--- namenode/namenode.go
+++ namenode/namenode.go
@@ -1,1 +1,14 @@
 package namenode
+
+type NameNodeService struct {
+	BlockSize uint64
+	ReplicationFactor uint64
+	Port uint64
+}
+
+func New(blockSize uint64, replicationFactor uint64) *NameNodeService {
+	return &NameNodeService{
+		BlockSize: blockSize,
+		ReplicationFactor: replicationFactor,
+	}
+}
