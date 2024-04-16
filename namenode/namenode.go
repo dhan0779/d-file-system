2f298a59714a257e6e63b4e80eff5024706805d2
David Han <dmh338@cornell.edu>
2024-04-16 16:36:34 -0400

    Initialize namenode function

--- namenode/namenode.go
+++ namenode/namenode.go
@@ -1,5 +1,12 @@
 package namenode
 
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"strconv"
+)
+
 type NameNodeService struct {
 	BlockSize uint64
 	ReplicationFactor uint64
@@ -11,4 +18,24 @@ func New(blockSize uint64, replicationFactor uint64) *NameNodeService {
 		BlockSize: blockSize,
 		ReplicationFactor: replicationFactor,
 	}
+}
+
+func Initialize(port int) {
+	instance := new(NameNodeService)
+	instance.Port = uint64(port)
+
+	err := rpc.Register(instance)
+	if err != nil {
+		panic(err)
+	}
+
+	rpc.HandleHTTP()
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+
+	if err != nil {
+		panic(err)
+	}
+
+	rpc.Accept(listener)
+	log.Println("datanode started on port: " + strconv.Itoa(port))
 }
