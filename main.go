e8b085603c2c3f6447282c4dc12d0f6359550699
David Han <dmh338@cornell.edu>
2024-04-16 16:36:55 -0400

    Main file for running servers

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import (
 	"d-file-system/datanode"
-	"d-file-system/namenode"
+	"log"
+	"os"
+	"strconv"
 )
 
+func main() {
+	switch os.Args[1] {
+	case "datanode":
+		port, e := strconv.Atoi(os.Args[2])
+		if e != nil {
+			log.Println("Error converting port number")
+			os.Exit(1)
+		}
+		datanode.Initialize(port)
+	default:
+		log.Println("Node not specified")
+		os.Exit(1)
+	}
+}
